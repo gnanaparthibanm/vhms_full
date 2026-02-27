54349d107e59596b1c20319d9faf9881b570d2b4
Gnanaparthiban M <gnanaparthiban9655@gmail.com>
2026-02-27 12:21:21 +0530

    bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'https://vhms-full.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
